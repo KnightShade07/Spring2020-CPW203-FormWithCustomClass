cf5b86a4dee2732811665ac44a752a9725952ddb
KnightShade085 <ethen.oliva@yahoo.com>
2020-05-20 20:33:07 -0700

    Displayed added games on webpage.

--- ts/main.ts
+++ ts/main.ts
@@ -69,6 +69,31 @@ function getVideoGame(): VideoGame{
 
 
 function displayGame(myGame: VideoGame): void {
+    let displayDiv = getById("display");
+
+    //create h2 with game title
+    let gameHeading = document.createElement("h2");
+    gameHeading.innerText = myGame.title;
+
+    //create paragraph with game details
+    let gameInfo = document.createElement("p");
+    let gameMediumDisplay = ""
+    if(myGame.isDigitalOnly){
+        gameMediumDisplay = "This is a digital only game";
+
+    }
+    else{
+        gameMediumDisplay = "You can come buy a physical copy!";
+    }
+    
+
+    gameInfo.innerText = `${myGame.title} has a rating of ${myGame.rating}.
+    it costs $${myGame.price.toFixed(2)}. ${gameMediumDisplay}`;
+
+    //add <h2> in the div display
+    displayDiv.appendChild(gameHeading);
+    //Add game info
+    displayDiv.appendChild(gameInfo);
 
 }
 
